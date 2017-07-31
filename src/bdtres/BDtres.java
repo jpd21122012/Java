6330c2b1d4ba50f0dbb9e2151192d507953d5b74
jpd21122012 <slipknot_jpd@hotmail.com>
2017-07-31 14:37:14 -0500

    Creacion de proyecto java

---
+++ src/bdtres/BDtres.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bdtres;
+
+/**
+ *
+ * @author Alumnos
+ */
+public class BDtres {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Ventana0 ventanaCero = new Ventana0();
+        ventanaCero.setVisible(true);
+    }
+}
